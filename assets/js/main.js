a0adef329f23e01d79d136196fc82d72b22970c4
Nesterov Andrey <polyshinnel@gmail.com>
2022-03-15 17:43:52 +0300

    add all pages,add inputmask,add recall-form

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,26 @@ $('.as-panel-wrapper').hover(function(){
 },function(){
     $(this).find('.title-block').fadeOut(300);
 });
+
+$('.header-btn-recall').click(function(){
+    $('.recall-wrapper').fadeIn(300);
+});
+
+$('.close-block').click(function(){
+    $('.recall-wrapper').fadeOut(300);
+});
+
+$(document).mouseup(function(e){
+    var recall = $('.recall-window');
+    var state = $('.recall-wrapper').css('display');
+    if(state == 'block'){
+        if (!recall.is(e.target) && recall.has(e.target).length === 0 ){
+            $('.recall-wrapper').fadeOut(300);
+        }
+    }
+    
+});
+
+$('#recall-phone').inputmask("+7(999)-999-99-99");
+
+$('#recall-window-time').inputmask("+7(999)-999-99-99");
